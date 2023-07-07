6cfc0f4e2ce9b8243b3494d1c075359767c57590
ernestoserv <ernesto.servine@gmail.com>
2023-07-06 18:58:05 -0600

    2.0 were the prediction in points, position and wins for this season is doisplayed

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from json import loads, dumps
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 def initialize():
     url = "http://ergast.com/api/f1/current/driverStandings.json"
     response = requests.get(url=url)
@@ -22,7 +25,8 @@ def initialize():
          "Driver": driver_info["code"],
           "Constructor": constructor_info["name"],
           "Season": season,
-          "Week": round
+          "Week": round,
+          "Driver_Season": str(season) + driver_info['code']
 
              }
 
@@ -49,13 +53,14 @@ def populate_dataframe(calc:int,stage:int,df,year=2023):
                 "Driver": driver_info["code"],
                 "Constructor": constructor_info["name"],
                 "Season": season,
-                "Week":stage
+                "Week":stage,
+                "Driver_Season": str(i) + driver_info['code']
             }
             new_row_df = pd.DataFrame(new_row, index=[0])
             df = pd.concat([df, new_row_df], ignore_index=True)
     return df
 
-standings = populate_dataframe(4, stage=round, df=temporada_actual)
+standings = populate_dataframe(10, stage=round, df=temporada_actual)
 standings['Position'] = pd.to_numeric(standings['Position'])
 standings["Points"] = pd.to_numeric(standings['Points'])
 standings["Wins"] = pd.to_numeric(standings['Wins'])
@@ -75,51 +80,51 @@ def end_of_season(calc:int,year=2023):
             constructor_info = entry["Constructors"][0]
 
             new_row = {
-                "Position": entry["position"],
-                "Points": entry["points"],
-                "Wins": entry["wins"],
+                "Position_EOS": entry["position"],
+                "Points_EOS": entry["points"],
+                "Wins_EOS": entry["wins"],
                 "Driver": driver_info["code"],
-                "Constructor": constructor_info["name"],
                 "Season": i,
-                "Week": 'Last Week of Season'
+                "Driver_Season": str(i) + driver_info['code']
             }
             new_row_df = pd.DataFrame(new_row, index=[0])
             df = pd.concat([df, new_row_df], ignore_index=True)
     return df
 
-eos = end_of_season(4)
-eos['Position'] = pd.to_numeric(eos['Position'])
-eos["Points"] = pd.to_numeric(eos['Points'])
-eos["Wins"] = pd.to_numeric(eos['Wins'])
+eos = end_of_season(10)
+eos['Position_EOS'] = pd.to_numeric(eos['Position_EOS'])
+eos["Points_EOS"] = pd.to_numeric(eos['Points_EOS'])
+eos["Wins_EOS"] = pd.to_numeric(eos['Wins_EOS'])
 eos['Driver'] = eos['Driver'].astype(str)
-eos['Constructor'] = eos['Constructor'].astype(str)
-top_ten_eos = eos[eos['Position'] <= 10]
-final_test = pd.concat([top_ten_per_season,top_ten_eos],ignore_index=True)
-fig, ax = plt.subplots(1,5,figsize=(22,7))
-fig.suptitle(f'End of Season Standing vs Standing of Week {round} over the past 4 seasons')
+#top_ten_eos = eos[eos['Position'] <= 10]
+final_test = standings[standings['Season'] != 2023].merge(eos,how='left',on='Driver_Season')
+X = final_test[['Position','Points',"Wins"]]
+y = final_test[['Position_EOS','Points_EOS','Wins_EOS']]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+regr = LinearRegression()
+regr.fit(X_train, y_train)
+print("R-squared score: ", regr.score(X_test, y_test))
+# Predict on the test set
+y_pred = regr.predict(temporada_actual[['Position','Points','Wins']])
+temporada_actual['Pred_Position'] = y_pred[:,0]
+temporada_actual['Pred_Points'] = y_pred[:,1]
+temporada_actual['Pred_Wins'] = y_pred[:,2]
+fig, ax = plt.subplots(1,3,figsize=(15,10))
+fig.suptitle(f'Current Points, Position and Wins vs Projected Points, Position and Wins')
 sns.set_style('whitegrid')
-ax[0].set_title('2019 Season')
-ax[1].set_title('2020 Season')
-ax[2].set_title('2021 Season')
-ax[3].set_title('2022 Season')
-ax[4].set_title('2023 Season')
-sns.barplot(data=final_test[final_test['Season'] == 2019],y='Driver',x='Position',hue='Week',ax=ax[0],orient='h',palette='Blues')
-sns.barplot(data=final_test[final_test['Season'] == 2020],y='Driver',x='Position',hue='Week',ax=ax[1],orient='h',palette='Reds')
-sns.barplot(data=final_test[final_test['Season'] == 2021],y='Driver',x='Position',hue='Week',ax=ax[2],orient='h',palette='RdBu')
-sns.barplot(data=final_test[final_test['Season'] == 2022],y='Driver',x='Position',hue='Week',ax=ax[3],orient='h',palette='Greens')
-sns.barplot(data=final_test[final_test['Season'] == 2023],y='Driver',x='Position',hue='Week',ax=ax[4],orient='h',palette='Purples')
-ax[0].bar_label(ax[0].containers[0])
-ax[0].bar_label(ax[0].containers[1])
-ax[1].bar_label(ax[1].containers[0])
-ax[1].bar_label(ax[1].containers[1])
-ax[2].bar_label(ax[2].containers[0])
-ax[2].bar_label(ax[2].containers[1])
-ax[3].bar_label(ax[3].containers[0])
-ax[3].bar_label(ax[3].containers[1])
-ax[4].bar_label(ax[4].containers[0])
+sns.scatterplot(temporada_actual,x='Points', y ='Pred_Points',
+                hue='Driver', style='Constructor',palette='pastel',s=200,ax=ax[0])
+sns.scatterplot(temporada_actual,x='Position', y ='Pred_Position',
+                hue='Driver', style='Constructor',palette='pastel',s=200,ax=ax[1])
+sns.scatterplot(temporada_actual,x='Wins', y ='Pred_Wins',
+                hue='Driver', style='Constructor',palette='pastel',s=200,ax=ax[2])
 
-
-
-
+#for i in range(len(temporada_actual)):
+   # ax.text(temporada_actual['Points'].iloc[i], temporada_actual['Pred_Points'].iloc[i], temporada_actual['Driver'].iloc[i],
+    #        )
+ax[0].set_ylim(0, 350)
 plt.show()
-print(final_test.head())
+
+
+
+
